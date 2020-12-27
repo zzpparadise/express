aaecf52bf2412e37ed7bc4f18de29135dba1ea87
zzpparadise <2523971057@qq.com>
2020-12-27 14:44:51 +0800

    测试

---
+++ src/main/java/com/zzp/test/Test1.java
@@ -0,0 +1,24 @@
+package com.zzp.test;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.zzp.dao.UserDao;
+import com.zzp.pojo.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:springConfig.xml")
+public class Test1 {
+    @Autowired
+    UserDao dao;
+    @Test
+    public void test() {
+        User user = dao.findUserByTel("999999999");
+        System.out.println(user);
+    }
+
+}
